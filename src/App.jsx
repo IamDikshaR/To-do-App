0d35ea8b8acfc493d397fadb0c0eb819da675920
diksha <rajdiksha91@gmail.com>
2024-03-12 23:22:51 +0530

    submitting todo items

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
+import Todo from "./components/Todo";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  );
 }
 
 export default App;
