2bc2dc8b29874f54b827bcd3c717e3f7b555cf2b
diksha <rajdiksha91@gmail.com>
2024-03-13 23:45:04 +0530

    using css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
+import "./app.css";
 import Todo from "./components/Todo";
+import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <Header />
       <Todo />
     </div>
   );
